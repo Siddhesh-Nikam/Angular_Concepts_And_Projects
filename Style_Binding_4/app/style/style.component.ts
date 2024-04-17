24792218d57c2e3a4d9ed0dcadbaa721fc5b0992
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-04-17 10:33:46 +0530

    Add files via upload

---
+++ Style_Binding_4/app/style/style.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style',
+  standalone: true,
+  imports: [],
+  templateUrl: './style.component.html',
+  styleUrl: './style.component.css'
+})
+export class StyleComponent 
+{
+    public MyColor = "pink";
+
+    public Isset = true;
+
+
+}
